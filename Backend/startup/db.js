69a172c03fbe96afbb30417317707ccbefd5483b
Sylvia Tian <sylviatian935@gmail.com>
2020-04-14 20:25:43 -0400

    set up backend

--- Backend/startup/db.js
+++ Backend/startup/db.js
@@ -3,6 +3,8 @@ const winston = require('winston');
 const config = require('config');
 
 module.exports = function () {
-    mongoose.connect(config.get('db'), { useNewUrlParser: true })
+    // mongoose.connect(config.get('db'), { useNewUrlParser: true })
+mongoose.connect('mongodb+srv://user1:<password>@volition-user-account-ullv2.mongodb.net/test?retryWrites=true&w=majority')
+    
     .then(() => winston.info('Connection to MongoDB established successfully...'));
 }
