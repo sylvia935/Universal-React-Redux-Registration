bf757e9a66700228813157071ed00194baf16b9b
Sylvia Tian <sylviatian935@gmail.com>
2020-04-14 20:51:47 -0400

    bcrypt issue

--- Backend/startup/db.js
+++ Backend/startup/db.js
@@ -4,7 +4,7 @@ const config = require('config');
 
 module.exports = function () {
     // mongoose.connect(config.get('db'), { useNewUrlParser: true })
-mongoose.connect('mongodb+srv://user1:<password>@volition-user-account-ullv2.mongodb.net/test?retryWrites=true&w=majority')
-    
+mongoose.connect('mongodb+srv://user1:volition123456@volition-user-account-ullv2.mongodb.net/test?retryWrites=true&w=majority')
+
     .then(() => winston.info('Connection to MongoDB established successfully...'));
 }
